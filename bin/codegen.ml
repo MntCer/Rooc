54a18aa0be046898dfbb9ce2f53467f7d890ea40
Xinyang Zhao <xinyang.zhao@tufts.edu>
2023-11-09 17:58:59 -0500

    codegen ready to merge

--- bin/codegen.ml
+++ bin/codegen.ml
@@ -11,7 +11,8 @@ let translate (functions, traits, structs, impls) =
 
   (* Add types *)
   (* TODO *)
-  let i32t_t     = L.i32_type    context
+  let i32_t     = L.i32_type    context
+  let i8_t      = L.i8_type     context
   (* ... *)
 
   let the_module = L.create_module context "Rooc" in
@@ -34,6 +35,11 @@ let translate (functions, traits, structs, impls) =
   (* TODO *)
   (* record implmentations *)
   StringMap.empty in
+  
+  let printf_t : L.lltype =
+      L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in 
+  let printf_func : L.llvalue =
+     L.declare_function "printf" printf_t the_module in
 
   let function_decls : (L.llvalue * sfunc_decl) StringMap.t =
   (* record function decls *)
@@ -47,6 +53,9 @@ let translate (functions, traits, structs, impls) =
 
   let build_function_body fdecl = 
 
+    let (the_function, _) = StringMap.find fdecl.sfname function_decls in
+    let builder = L.builder_at_end context (L.entry_block the_function) in
+
     let rec expr builder ((_, e) : sexpr) = match e with
         SLiteral i -> L.const_int i32_t i
       | SBoolLit b -> L.const_int i1_t (if b then 1 else 0)
@@ -55,16 +64,72 @@ let translate (functions, traits, structs, impls) =
       | SId s -> L.build_load (lookup s) s builder
       | SAssign (s, e) -> let e' = expr builder e in
                          let _  = L.build_store e' (lookup s) builder in e'
-      (* TODO *)
+      | SBinop (e1, op, e2) ->
+          let (t, _) = e1
+          and e1' = expr builder e1
+          and e2' = expr builder e2 in
+          if t = A.Float then (match op with
+            A.Add     -> L.build_fadd
+          | A.Sub     -> L.build_fsub
+          | A.Mult    -> L.build_fmul
+          | A.Div     -> L.build_fdiv
+          | A.Equal   -> L.build_fcmp L.Fcmp.Oeq
+          | A.Neq     -> L.build_fcmp L.Fcmp.One
+          | A.Less    -> L.build_fcmp L.Fcmp.Olt
+          | A.Leq     -> L.build_fcmp L.Fcmp.Ole
+          | A.Greater -> L.build_fcmp L.Fcmp.Ogt
+          | A.Geq     -> L.build_fcmp L.Fcmp.Oge
+          | A.And | A.Or ->
+              raise (Failure "internal error: semant should have rejected and/or on float")
+          ) e1' e2' "tmp" builder
+          else (match op with
+          | A.Add     -> L.build_add
+          | A.Sub     -> L.build_sub
+          | A.Mult    -> L.build_mul
+          | A.Div     -> L.build_sdiv
+          | A.And     -> L.build_and
+          | A.Or      -> L.build_or
+          | A.Equal   -> L.build_icmp L.Icmp.Eq
+          | A.Neq     -> L.build_icmp L.Icmp.Ne
+          | A.Less    -> L.build_icmp L.Icmp.Slt
+          | A.Leq     -> L.build_icmp L.Icmp.Sle
+          | A.Greater -> L.build_icmp L.Icmp.Sgt
+          | A.Geq     -> L.build_icmp L.Icmp.Sge
+          ) e1' e2' "tmp" builder
+      | SUnop(op, e) ->
+          let (t, _) = e in
+          let e' = expr builder e in
+          (match op with
+            A.Neg when t = A.Float -> L.build_fneg
+          | A.Neg                  -> L.build_neg
+          | A.Not                  -> L.build_not) e' "tmp" builder
+      | SCall ("print_str", [e]) -> 
+          L.build_call printf_func [| (expr builder e) |] "printf" builder
+      | _ -> L.const_int i32_t 0
+      
+      (* TODO: add function call, member *)
       (* ... *)
+
+    let add_terminal builder instr =
+                           (* The current block where we're inserting instr *)
+      match L.block_terminator (L.insertion_block builder) with
+        Some _ -> ()
+      | None -> ignore (instr builder) in
 
     let rec stmt builder = function
         SBlock sl -> List.fold_left stmt builder sl
       (* TODO *)
       (* ... *)
-      | _ -> builder
-  
+      | _ -> builder in
+    let builder = stmt builder (SBlock fdecl.fd_body) in
+    add_terminal builder (match fdecl.styp with
+        A.Void -> L.build_ret_void
+      | A.Float -> L.build_ret (L.const_float float_t 0.0)
+      | t -> L.build_ret (L.const_int (ltype_of_typ t) 0))
+  in
+
   let build_method_body idecl = 
+    List.iter build_function_body idecl.i_methods
     (* TODO: deal with implementations' methods *)  
   in
   List.iter build_function_body functions;
