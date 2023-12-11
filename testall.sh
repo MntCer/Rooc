6ca882448110b75c0b864618ba4397583467c6eb
MonaMaNotAvailable <zma4@wpi.edu>
2023-12-10 22:03:32 -0500

    [WIP] slightly modify testall.sh to keep intermediate .ll files

--- testall.sh
+++ testall.sh
@@ -83,8 +83,9 @@ Check() {
     echo "###### Testing $basename" 1>&2
 
     generatedfiles=""
+    keepfiles="${basename}.ll" #keep all the .ll files automatically
 
-    generatedfiles="$generatedfiles ${basename}.ll ${basename}.s ${basename}.exe ${basename}.out" &&
+    generatedfiles="$generatedfiles ${basename}.s ${basename}.exe ${basename}.out" &&
     Run "dune exec $Rooc" "$1" ">" "${basename}.ll" && # generate result
     Run "$LLC" "-relocation-model=pic" "${basename}.ll" ">" "${basename}.s" &&
     Run "$CC" "-o" "${basename}.exe" "${basename}.s" &&
